cd8a8f275899dd9554456b7775e0eabf7816cf04
dcirer <domy.741@gmail.com>
2024-10-19 14:12:52 -0300

    operadores

---
+++ 02-operadores.py
@@ -0,0 +1,27 @@
+# operadores aritmeticos#
+
+print(3+4)
+print(3-4)
+print(3/4)
+print(3*4)
+print(10%3)
+print(10//3)  #redondea el resultado de la division
+print(2**3)
+
+print("Hola" + "Python")
+print("Hola " + str(5))
+print("Hola " * 5)
+
+# operadores comparativos#
+print(3>4)
+print(3<4)
+print(3>=4)
+print(3<=4)
+print(3==4)
+print(3!=4)
+
+# operadores logicos#
+
+print(3>4 and "Hola">"Python")#dos falsos
+print(3>4 or "Hola">"Python")
+print(not(3<4))
