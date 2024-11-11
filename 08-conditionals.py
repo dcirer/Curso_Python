44f3ef211dd50cdb260af67e034f93643a054f54
dcirer <domy.741@gmail.com>
2024-11-11 09:46:05 -0300

    condicionales

---
+++ 08-conditionals.py
@@ -0,0 +1,30 @@
+#conditionals#
+
+my_condition = True
+
+if my_condition:
+    print("se ejecuta la condicion del if")
+
+my_condition = 5*2
+
+if my_condition <10:
+    print("se ejecuta la segunda condicion del if")
+
+if my_condition >=10:
+    print("se ejecuta la tercer condicion del if")
+    
+
+#ELSE
+
+if my_condition >10:
+    print("Es mayor que 10")
+else:
+    print("es menor igual a 10")
+
+#cadenas de texto vacias genera un false
+
+my_string="Mi cadena de texto"
+
+if my_string:
+    print("Mi cadena de texto no es vacia")
+
